06979c5978178f9c706cfeac32eeef0b5ecd1823
Lucas Mesquita <lucasoliveira98@outlook.com>
2022-03-01 14:40:33 -0300

    fix: impoving T constraint

--- packages/section7/index.ts
+++ packages/section7/index.ts
@@ -1,5 +1,8 @@
 // Generics, constraints with "extends" and keyof
-function extractValueByKey<T, U extends keyof T>(obj: T, key: U) {
+function extractValueByKey<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
   return obj[key];
 }
 console.log(extractValueByKey({ name: "Lucas", age: 24 }, "age"));
